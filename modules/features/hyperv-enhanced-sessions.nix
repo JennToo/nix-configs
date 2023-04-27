4249fe2b26dc6d13b7a2efb88fcaaf0f38846f1d
Jennifer Wilcox <jennifer@nitori.org>
2023-04-27 15:34:54 -0500

    Enable XFCE4 session for Hyper-V
    Gnome is extremely slow and buggy over the RDP connection.

--- modules/features/hyperv-enhanced-sessions.nix
+++ modules/features/hyperv-enhanced-sessions.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }: {
   imports = [ ../programs/xrdp-hyperv.nix ];
   services.xrdp-hyperv.enable = true;
+  services.xrdp-hyperv.defaultWindowManager = "xfce4-session";
   boot.kernelModules = [ "hv_sock" ];
 
   services.xrdp-hyperv.package = pkgs.xrdp.overrideDerivation
